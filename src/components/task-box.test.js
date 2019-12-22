ed9737bfa579d7e99f5bb1a238fc4fe94c09e3bd
Филипп <filipp@MacBook-Pro-Filipp.local>
2019-12-22 13:50:43 +0300

    Initial commit

--- src/components/task-box.test.js
+++ src/components/task-box.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import TaskBox from './task-box';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<TaskBox />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
---
+++ src/redux/store.js
@@ -0,0 +1,4 @@
+import { combineReducers, createStore } from "redux";
+import { tasks, UI } from "./reducer";
+
+export default createStore(combineReducers({ tasks, UI }));
