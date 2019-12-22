f5d2b31b175c1192ce6571e8f2caec2e3484ecaf
Филипп <filipp@MacBook-Pro-Filipp.local>
2019-12-22 14:06:52 +0300

    Add relative paths to build

---
+++ build/precache-manifest.ff8510599dad184557c5be82bc2e2175.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "084317f8efa1f9c666856b3daa8c8747",
+    "url": "tascana-web/build/index.html"
+  },
+  {
+    "revision": "bb0bfa4e68a81b158bf5",
+    "url": "tascana-web/build/static/css/main.880821b0.chunk.css"
+  },
+  {
+    "revision": "f044e2b31648781b401b",
+    "url": "tascana-web/build/static/js/2.e110f164.chunk.js"
+  },
+  {
+    "revision": "bb0bfa4e68a81b158bf5",
+    "url": "tascana-web/build/static/js/main.48e7c1d8.chunk.js"
+  },
+  {
+    "revision": "3c63aab5d7b0590376ee",
+    "url": "tascana-web/build/static/js/runtime-main.377b89ef.js"
+  }
+]);
