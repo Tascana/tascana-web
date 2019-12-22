2983c2c1c3511d06ff5ef4a2b985a910ac8b691e
Филипп <filipp@MacBook-Pro-Filipp.local>
2019-12-22 13:51:40 +0300

    Add build folder

---
+++ build/precache-manifest.cdb3c9717c5ab3ba24e39da806722dea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfd7dd8649e502afbe3f5d2648c2b4cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33de5fc1b6a5f6407790",
+    "url": "/static/css/main.880821b0.chunk.css"
+  },
+  {
+    "revision": "f044e2b31648781b401b",
+    "url": "/static/js/2.e110f164.chunk.js"
+  },
+  {
+    "revision": "33de5fc1b6a5f6407790",
+    "url": "/static/js/main.99170e92.chunk.js"
+  },
+  {
+    "revision": "fe5c7aae141f03b42984",
+    "url": "/static/js/runtime-main.8eea71de.js"
+  }
+]);
