158019ff3ec143a90e4ed242a6bcb713a8894611
Danil Shashkov <shashkovdanil@gmail.com>
2019-12-25 14:02:48 +0500

    Возможные фиксы infinite-scroll

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import store from './redux/store'
-import './index.css';
-import * as serviceWorker from './serviceWorker';
-import App from './components/app';
+import './index.css'
+import * as serviceWorker from './serviceWorker'
+import App from './components/app'
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
