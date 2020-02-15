3a066d168da60de285ad4de765ea48992c669ae3
shashkovdanil <shashkovdanil@gmail.com>
2020-02-16 02:41:46 +0500

    #26 Save gradient in firebase

--- src/redux/tasks.js
+++ src/redux/tasks.js
@@ -1,7 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit'
 import omit from 'lodash/omit'
 import nanoid from 'nanoid'
-import { MONTH, YEAR } from '../constants/task-types'
+import { YEAR, MONTH } from '../constants/task-types'
 import { randomGrad } from '../components/Tasks/utils'
 import uiSlice from './UI'
 
@@ -46,52 +46,49 @@ const tasksSlice = createSlice({
   reducers: {
     setTasks: (state, { payload: tasks }) => {
       const tasksArray = Array.isArray(tasks) ? tasks : Object.values(tasks)
-      const computedTasksArray = tasksArray.map((task, index, arr) => {
-        const unassignedGradient = 'linear-gradient(to bottom, #e2e2e2, #bbb)'
+      const computedTasksArray = tasksArray
+        .sort((a, b) => a.position - b.position)
+        .map((task, index, arr) => {
+          const unassignedGradient = 'linear-gradient(to bottom, #e2e2e2, #bbb)'
 
-        const { parents, children } = getTree(tasks, task)
+          const { parents, children } = getTree(tasks, task)
 
-        function getGradient() {
-          const sortedTasks = arr
-            .slice()
-            .sort((a, b) => a.position - b.position)
+          function getGradient() {
+            if (task.type === YEAR) {
+              return randomGrad(index)
+            }
 
-          if (task.type === YEAR)
-            return randomGrad(sortedTasks.findIndex(t => t.id === task.id))
+            const yearLevelParent = parents.find(p => p.type === YEAR)
+            const monthLevelParent = parents.find(p => p.type === MONTH)
 
-          const yearLevelParent = parents.find(p => p.type === YEAR)
-          const monthLevelParent = parents.find(p => p.type === MONTH)
+            if (yearLevelParent)
+              return randomGrad(arr.findIndex(t => t.id === yearLevelParent.id))
+            if (monthLevelParent)
+              return randomGrad(
+                arr.findIndex(t => t.id === monthLevelParent.id),
+              )
 
-          if (yearLevelParent)
-            return randomGrad(
-              sortedTasks.findIndex(t => t.id === yearLevelParent.id),
-            )
-          if (monthLevelParent)
-            return randomGrad(
-              sortedTasks.findIndex(t => t.id === monthLevelParent.id),
-            )
+            return unassignedGradient
+          }
 
-          return unassignedGradient
-        }
+          function getProgress() {
+            return children.filter(c => c.done).length
+              ? (children.filter(c => c.done).length / children.length) * 100
+              : 0
+          }
 
-        function getProgress() {
-          return children.filter(c => c.done).length
-            ? (children.filter(c => c.done).length / children.length) * 100
-            : 0
-        }
+          return {
+            ...task,
+            parents,
+            children,
+            done: getProgress() === 100 ? true : task.done,
+            backgroundGradient: task.backgroundGradient || getGradient(),
+            position: typeof task.position !== 'number' ? index : task.position,
+            progress: getProgress(),
+          }
+        })
 
-        return {
-          ...task,
-          parents,
-          children,
-          done: getProgress() === 100 ? true : task.done,
-          backgroundGradient: getGradient(),
-          position: typeof task.position !== 'number' ? index : task.position,
-          progress: getProgress(),
-        }
-      })
-
-      return computedTasksArray.sort((a, b) => a.position - b.position)
+      return computedTasksArray
     },
     createTask: (state, action) => ({
       ...state,
@@ -156,6 +153,16 @@ export const createTaskAction = ({
       return filteredTasks.length
     }
 
+    const position = getPosition()
+
+    function getGradient() {
+      if (parent) {
+        return tasks.find(t => t.id === parent).backgroundGradient
+      }
+
+      return randomGrad(position)
+    }
+
     const newTaskId = nanoid(12)
     const newTask = {
       task: text,
@@ -167,8 +174,9 @@ export const createTaskAction = ({
       year: id.year,
       month: id.month || -1,
       day: id.day || -1,
-      parentId: parent || null,
-      position: getPosition(),
+      parentId: type !== YEAR && parent ? parent : null,
+      position,
+      backgroundGradient: getGradient(),
       userId,
       createdAt: Date.now(),
       updatedAt: -1,
