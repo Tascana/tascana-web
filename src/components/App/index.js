0cafb57c8aa332372dd4dafbb2b8fa7e1193bd5d
Danil Shashkov <shashkovdanil@gmail.com>
2020-02-24 13:59:48 +0500

    Remove test swipe component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import IndexPage from '../../pages/index'
 import YearPage from '../../pages/year'
-import Test from '../Test'
 import Landing from '../Landing'
 import useAuthentication from '../../hooks/use-authentication'
 
@@ -14,7 +13,6 @@ function App() {
       <Switch>
         <Route exact path="/" component={IndexPage} />
         <Route exact path="/signin" component={Landing} />
-        <Route exact path="/test" component={Test} />
         <Route path="/:year" component={YearPage} />
       </Switch>
     </Router>
