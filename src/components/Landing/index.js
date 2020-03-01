9f4a2d81386e31b838093a95e7b203ebb7bfa4ea
Nikita Volkov <macfire10@gmail.com>
2020-03-02 00:41:17 +0300

    responsive layout tweaks

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -8,40 +8,6 @@ import { YearGoalsDemo } from './YearGoalsDemo'
 import { MonthGoalsDemo } from './MonthGoalsDemo'
 import { DayGoalsDemo } from './DayGoalsDemo'
 
-  function showAnimation(timestamp) {
-    if (!start) {
-      start = timestamp || new Date().getTime()
-    }
-
-    let elapsed = timestamp - start
-    let progress = elapsed / 600
-
-    const outQuad = function(n) {
-      return n * (2 - n)
-    }
-
-    let easeInPercentage = +outQuad(progress).toFixed(2)
-
-    pos =
-      target === 0
-        ? firstPos - firstPos * easeInPercentage
-        : firstPos + target * easeInPercentage
-
-    window.scrollTo(0, pos)
-
-    if (
-      (target !== 0 && pos >= firstPos + target) ||
-      (target === 0 && pos <= 0)
-    ) {
-      cancelAnimationFrame(start)
-      pos = 0
-    } else {
-      window.requestAnimationFrame(showAnimation)
-    }
-  }
-  window.requestAnimationFrame(showAnimation)
-}
-
 function Landing() {
   const [error, setError] = useState(null)
   const [heightAnimation, setHeightAnimation] = useState(true)
