abff27f43399fb9ad8f058dc6e42df63bd6e0086
ifilipis <filipis96@gmail.com>
2021-11-04 21:54:04 +0500

    Fix deployment

--- src/services/firebase.tsx
+++ src/services/firebase.tsx
@@ -4,14 +4,14 @@ import 'firebase/database'
 import 'firebase/analytics'
 
 const config = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+  apiKey: window.__RUNTIME_CONFIG__.REACT_APP_API_KEY,
+  authDomain: window.__RUNTIME_CONFIG__.REACT_APP_AUTH_DOMAIN,
+  databaseURL: window.__RUNTIME_CONFIG__.REACT_APP_DATABASE_URL,
+  projectId: window.__RUNTIME_CONFIG__.REACT_APP_PROJECT_ID,
+  storageBucket: window.__RUNTIME_CONFIG__.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: window.__RUNTIME_CONFIG__.REACT_APP_MESSAGING_SENDER_ID,
+  appId: window.__RUNTIME_CONFIG__.REACT_APP_APP_ID,
+  measurementId: window.__RUNTIME_CONFIG__.REACT_APP_MEASUREMENT_ID,
 }
 
 export class Firebase {
