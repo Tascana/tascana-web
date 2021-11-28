a587ab3755f27eafad9067f9aa15497b3295ee4c
PhilippSytarskiy <philippsitarskiy@gmail.com>
2021-11-29 02:47:28 +0300

    bugfix Task is not deselected on the outside click

--- src/components/Lines/index.tsx
+++ src/components/Lines/index.tsx
@@ -3,15 +3,18 @@ import '../app.css'
 import React from 'react'
 import HorizontalUI from './HorizontalUI'
 import { useShortcutSwipe } from './useShortcutSwipe'
+import { useDispatch } from 'react-redux'
+import { selectTreeAction } from '../../redux/UI'
 import { YEAR, MONTH, DAY } from '../../constants/task-types'
 
 import styles from './styles.module.scss'
 
 function Lines() {
   const { onScroll, line } = useShortcutSwipe()
+  const dispatch = useDispatch()
 
   return (
-    <main className={styles.Wrapper}>
+    <main className={styles.Wrapper} onClick={() => dispatch(selectTreeAction({ todo: null }))}>
       <HorizontalUI type={YEAR} onScroll={onScroll} swipeableLine={line} />
       <HorizontalUI type={MONTH} onScroll={onScroll} swipeableLine={line} />
       <HorizontalUI type={DAY} onScroll={onScroll} swipeableLine={line} />
